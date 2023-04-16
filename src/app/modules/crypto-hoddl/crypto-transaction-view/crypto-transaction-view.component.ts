589053e132c9245035754a48b530bc54fb14d4d5
Mario Bittner <MarioBittner@gmx.de>
2023-04-17 00:14:52 +0200

    adding a Transaction Table using div for all areas

--- src/app/modules/crypto-hoddl/crypto-transaction-view/crypto-transaction-view.component.ts
+++ src/app/modules/crypto-hoddl/crypto-transaction-view/crypto-transaction-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReportEntry } from '../crypto.model';
 
 @Component({
   selector: 'crypto-transaction-view',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./crypto-transaction-view.component.scss']
 })
 export class CryptoTransactionViewComponent {
-
+  displayedColumns = ['Value', 'Asset', 'InDate', 'OutDate'];
+  transactions: ReportEntry[] = [{
+    "Value": 10,
+    "Asset": "BTC",
+    "InDate": "04/04/2020",
+    "OutDate": "05/05/2020",
+    "SL": "short",
+    "Proceed": "",
+    "Costbase": 0.45,
+    "Profit": 20
+  }, {
+    "Value": 22,
+    "Asset": "ETH",
+    "InDate": "04/04/2020",
+    "OutDate": "05/05/2020",
+    "SL": "short",
+    "Proceed": "",
+    "Costbase": 0.45,
+    "Profit": 29
+  }, {
+    "Value": 33,
+    "Asset": "BTC",
+    "InDate": "04/04/2020",
+    "OutDate": "05/05/2020",
+    "SL": "short",
+    "Proceed": "",
+    "Costbase": 0.35,
+    "Profit": 10
+  }];
 }
